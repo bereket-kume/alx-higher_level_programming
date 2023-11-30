61b6a4a1a3f3db1fda992a791d939c876d5d3201
bereket-kume <bereketkume@gmail.com>
2023-11-30 11:05:46 -0800

    done

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
 import sys
-def args(argv):
-    l = argv.split()
-    if l == 0:
-        print("{:d} arguments.".format(l))
-    else:
-        for i , arg in enumerate(argv):
-            print("{:d}: {}".format(i + 1, arg))
+if __name == "__main__":
+    exit()
+mess = "{} argument"
+arglen = len(sys.argv) - 1
+if arglen == 0:
+    mess += "s."
+elif arglen == 1:
+    mess += ":"
+else:
+    mess += "s:"
+print(mess.format(arglen))
+i = 0
+for arg in sys.argv:
+    if i != 0:
+        print("{:d} {}".format(i, arg)
+    i += 1
