d82afa9a3e13731cbab5630f8a4ae9d58121a997
bereket-kume <bereketkume@gmail.com>
2023-12-02 22:41:35 -0800

    fixed error

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -3,7 +3,6 @@ def replace_in_list(my_list, idx, element):
     for key, item in enumerate(my_list):
         if idx < 0 or idx >= len(my_list):
             return my_list
-        if idx == key:
-            my_list[key] = element
-        return my_list[key]
+        my_list[idx] = element
+        return my_list
         
