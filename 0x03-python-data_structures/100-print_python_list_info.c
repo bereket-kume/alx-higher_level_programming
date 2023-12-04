5a8180b0c3b1fc2fc91910c9a668a4bbbe86e564
bereket-kume <135522204+bereket-kume@users.noreply.github.com>
2023-12-04 11:23:39 -0800

    Update 100-print_python_list_info.c

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,1 +1,16 @@
-:
+#include <Python.h>
+
+void print_python_list_info(PyObject *p) {
+    PyListObject *list = (PyListObject *)p;
+    Py_ssize_t size = PyList_Size(p);
+    Py_ssize_t allocated = list->allocated;
+
+    printf("[*] Size of the Python List = %ld\n", size);
+    printf("[*] Allocated = %ld\n", allocated);
+
+    for (Py_ssize_t i = 0; i < size; i++) {
+        PyObject *item = PyList_GetItem(p, i);
+        const char *typeName = Py_TYPE(item)->tp_name;
+        printf("Element %ld: %s\n", i, typeName);
+    }
+}
