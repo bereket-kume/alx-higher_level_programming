d9157893a5e2da70e01456f66db5d50de199efdb
bereket-kume <135522204+bereket-kume@users.noreply.github.com>
2023-12-04 05:15:55 -0800

    Update 9-max_integer.py

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-def max_integer(my_list=[]):
+def max_integer(my_list=None):
+    my_list = my_list or []
     sorted_list = sorted(my_list)
     return sorted_list[-1] if sorted_list else None
