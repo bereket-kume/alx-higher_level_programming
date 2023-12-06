662222e4af5c889a9335489519f90389459ab9c7
bereket-kume <bereketkume@gmail.com>
2023-12-06 11:45:12 -0800

    done

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_list = my_list.copy()
-    for key, value enumerate(new_list):
+    for key, value in enumerate(new_list):
         if search == value:
             new_list[key] = replace
     return new_list
