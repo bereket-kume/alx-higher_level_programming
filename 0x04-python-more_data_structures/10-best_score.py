8fd58162afea0ed9109f8ce81599f4f78f980d2f
bereket-kume <bereketkume@gmail.com>
2023-12-07 01:33:09 -0800

    done

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary:
-        max_dict = max(a_dictionary, key = a_dictionary.get)
+        max_dict = max(a_dictionary, key=a_dictionary.get)
         return max_dict
     else:
         return None
