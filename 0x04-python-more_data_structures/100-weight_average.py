32c7fbb6cf1ecac4140569d3b9240769bebd48f9
bereket-kume <bereketkume@gmail.com>
2023-12-08 05:08:10 -0800

    done

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,6 +2,8 @@
 def weight_average(my_list=[]):
     n_sum = 0
     d_sum = 0
+    if len(my_list) == 0:
+        return 0
     for i in my_list:
         n_sum += i[0] * i[1]
         d_sum += i[1]
