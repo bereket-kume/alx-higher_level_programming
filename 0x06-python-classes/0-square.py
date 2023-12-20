f425b9ee7fae661e7834aa007b507f3df83ea650
bereket-kume <bereketkume@gmail.com>
2023-12-20 08:46:31 -0800

    done

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 class Square:
     """Defines a square"""
-    def __inti__(self, size=0,postion=(0,0)):
-        """Initialises the data"""
-        self.size = size
-        self.postion = postion
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+class Square:
+    """Defines a square"""
+    def __init__(self, size=0,postion=(0,0)):
+        self.size = size
+        self.postion = postion
