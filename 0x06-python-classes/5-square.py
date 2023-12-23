7679b7c76c0552b98c36ae9e0a51cfd1b993d216
bereket-kume <135522204+bereket-kume@users.noreply.github.com>
2023-12-23 03:51:05 -0800

    Create 5-square.py

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+"""
+This module contains the Square class
+"""
+
+
+class Square:
+    """
+    This class represents a square
+    """
+    def __init__(self, size=0):
+        """
+        Initializes a Square instance
+
+        Args:
+            size (int): The size of the square (default 0)
+        """
+        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """
+        Calculates and returns the area of the square
+
+        Returns:
+            int: The area of the square
+        """
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """
+        getter method
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        setter method
+        """
+        self.__size = value
+        if not isinstance(self.__size, int):
+            raise TypeError("size must be an integer")
+        if self.__size < 0:
+            raise ValueError("size must be >= 0")
+          
+    def my_print(self):
+        """
+        my_print method
+        """
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+              for j in range(self.__size):
+                print("#", end="")
+              print()
