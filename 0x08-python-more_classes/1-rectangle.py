cec69ff2ab65cfb9203b192260cbacc699406002
bereket-kume <bereketkume@gmail.com>
2024-01-05 08:15:59 -0800

    error is fixed

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -10,11 +10,27 @@ class Rectangle:
         """ intialize a Rectangle instance"""
         self.__width = width
         self.__height = height
-        if not isinstance(width, int):
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if isinstance(value, int):
             raise TypeError("width must be an integer")
-        if width < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
-        if not isinstance(height, int):
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        if height < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
+        self.__height = value
