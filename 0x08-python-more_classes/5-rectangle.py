b27612cdc55fdabf4de7ca46148da40287fa5084
bereket-kume <bereketkume@gmail.com>
2024-01-05 10:51:57 -0800

    done

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -52,5 +52,5 @@ class Rectangle:
     def __repr__(self):
         return "Rectangle({}, {})".format(self.__width, self.__height)
 
-    def __delete__(self):
+    def __del__(self):
         print("Bye rectangle...")
