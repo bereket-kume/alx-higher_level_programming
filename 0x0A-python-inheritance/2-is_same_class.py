a471472c66e88329d8b5b7e4b4f42429c754bb37
bereket-kume <bereketkume@gmail.com>
2024-01-08 11:51:07 -0800

    done

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""module"""
+"""module with is_same_class"""
 
 
 def is_same_class(obj, a_class):
+    """methhid that return true for the class"""
     return type(obj) is a_class
