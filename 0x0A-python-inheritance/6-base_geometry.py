f9042e6fd7dd17eb8dd5209c1d40317c51ef5ae6
bereket-kume <bereketkume@gmail.com>
2024-01-08 12:06:16 -0800

    done

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""module for BaseGeometry"""
+
+
+class BaseGeometry:
+    """area method container"""
+
+    def area(self):
+        """area method"""
+        raise Exception("area() is not implemented")
