90f59045ba5d766771b1c3d425ac8e8d861fbed9
bereket-kume <bereketkume@gmail.com>
2024-01-09 12:59:59 -0800

    done

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+"""module for read_file"""
 
 
 def read_file(filename=""):
+    """read_file method"""
+
     with open(filename, encoding="UTF-8") as file:
         content = file.read()
         print(content, end="")
