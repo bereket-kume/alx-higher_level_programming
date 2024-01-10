a9082c7a8102a75fc72b812ae54b3900e38b8789
bereket-kume <135522204+bereket-kume@users.noreply.github.com>
2024-01-10 14:09:37 -0800

    Create 100-append_after.py

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""module for append after"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+  """append after method"""
+  
+    with open(filename, 'r+') as file:
+        lines = file.readlines()
+        file.seek(0)
+        for line in lines:
+            file.write(line)
+            if search_string in line:
+                file.write(new_string)
+        file.truncate()
