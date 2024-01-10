31d9ea710367aa58d916f3ad132176aa3f9c00c7
bereket-kume <bereketkume@gmail.com>
2024-01-10 10:20:43 -0800

    done

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""module for add item"""
+
+
+import sys
+from typing import List
+from os.path import exists
+
+
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+
+
+filename = "add_item.json"
+
+if exists(filename):
+    my_list = load_from_json_file(filename)
+else:
+    my_list = []
+
+my_list.extend(sys.argv[1:])
+
+save_to_json_file(my_list, filename)
