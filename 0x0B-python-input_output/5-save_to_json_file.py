e548609e9c00f11e8cce0f814d3f6a882b41da8a
bereket-kume <bereketkume@gmail.com>
2024-01-10 09:53:12 -0800

    done

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -9,4 +9,4 @@ def save_to_json_file(my_obj, filename):
     """save to json method"""
 
     with open(filename, mode='w', encoding='utf-8') as f:
-        json.dump(my_obj, filename)
+        json.dump(my_obj, f)
