3a279b962b2c27dbabcfe827027e37824b1ffe64
bereket-kume <bereketkume@gmail.com>
2024-01-10 09:51:34 -0800

    done

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""module for save_to_json_file"""
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """save to json method"""
+
+    with open(filename, mode='w', encoding='utf-8') as f:
+        json.dump(my_obj, filename)
