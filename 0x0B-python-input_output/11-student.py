18336e7f3c5a75a05f7e8949bc12c37f1fe302de
bereket-kume <bereketkume@gmail.com>
2024-01-10 11:54:11 -0800

    done

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -7,6 +7,7 @@ class Student:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
+
     def to_json(self, attrs=None):
         if attrs is None:
             return self.__dict__
@@ -15,3 +16,6 @@ class Student:
             if hasattr(self, attr):
                 json_dict[attr] = getattr(self, attr)
         return json_dict
+    def reload_from_json(self, json):
+        for keys, value in json.items():
+            setarr(self,keys, value)
