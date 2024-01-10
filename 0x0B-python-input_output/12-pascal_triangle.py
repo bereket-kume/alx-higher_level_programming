6cfb08ee24cc7d190b624a74960e5824456a38f4
bereket-kume <135522204+bereket-kume@users.noreply.github.com>
2024-01-10 14:02:17 -0800

    Update 12-pascal_triangle.py

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
-"""module for pasacal triangle method"""
 
 
 def pascal_triangle(n):
     if n <= 0:
         return []
-    a = [[]for _ in range(n)]
-    for i in range(n):
-        for j in range(i + 1):
-            if j < i:
-                if j == 0:
-                    a[i].append(1)
-                else:
-                    a[i].append(a[i - 1][j] + a[i - 1][j - 1])
-            elif j == i:
-                a[i].append(1)
-        return a
+
+    triangle = [[1]]
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        row.append(1)
+        triangle.append(row)
+
+    return triangle
