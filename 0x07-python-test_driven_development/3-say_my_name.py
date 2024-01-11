1d0026ae16016f17ddbdf6997c03210b5162e3fa
bereket-kume <135522204+bereket-kume@users.noreply.github.com>
2024-01-11 09:48:09 -0800

    Update 3-say_my_name.py

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python3
-"""module for 3-say_my_name.py"""
+"""module for say_my_name method"""
 
 
 def say_my_name(first_name, last_name=""):
     """say my name method"""
-
+    
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
-
-    if last_name and not isinstance(last_name, str):
+    if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-
-    if last_name:
-        print("My Name is {:s} {:s}".format(first_name, last_name))
-    else:
-        print("My Name is {:s}".format(first_name))
+    print("My name is {:s} {:s}".format(first_name, last_name))
