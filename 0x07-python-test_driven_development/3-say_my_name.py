8e491ce07884c9fdb03adfa199d5d453d87fc445
bereket-kume <bereketkume@gmail.com>
2024-01-11 09:06:00 -0800

    done

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""module for 3-say_my_name.py"""
+
+
+def say_my_name(first_name, last_name=""):
+    """say my name method"""
+
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print("My Name is {:s} {:s} ".format(first_name, last_name))
