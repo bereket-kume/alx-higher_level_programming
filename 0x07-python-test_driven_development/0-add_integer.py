0237267e559b8f03af076064bcbc4c7af30afd8e
bereket-kume <bereketkume@gmail.com>
2024-01-11 07:28:56 -0800

    done

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""module for add interger method"""
+
+
 def add_integer(a, b=98):
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
