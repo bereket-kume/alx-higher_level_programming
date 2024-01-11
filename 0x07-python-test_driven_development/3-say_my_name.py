3b0f057e4b68a12458b7a2a473cb9fdf118afa7a
bereket-kume <bereketkume@gmail.com>
2024-01-11 09:49:55 -0800

    done

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -4,7 +4,7 @@
 
 def say_my_name(first_name, last_name=""):
     """say my name method"""
-    
+
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
