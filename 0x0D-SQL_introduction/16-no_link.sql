326f848b4dc28a460416c9f51b84d9cbd22abac6
bereket-kume <135522204+bereket-kume@users.noreply.github.com>
2024-03-12 14:57:33 -0700

    Update 16-no_link.sql

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,6 +1,6 @@
--- this script that lists all records of the table 
---second_table of the database hbtn_0c_0
+-- Lists all records of the table second_table having a name value.
+-- Records are ordered by descending score.
 SELECT `score`, `name`
 FROM `second_table`
 WHERE `name` != ""
-ORDER BY `score` DESC;
+ORDER BY `score` DESC
