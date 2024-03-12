d0ab0a503229dafa6ccf3ff510b1d692be6a145f
bereket-kume <135522204+bereket-kume@users.noreply.github.com>
2024-03-12 14:07:36 -0700

    Update 11-best_score.sql

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,6 +1,6 @@
 -- this script that ists all records 
 -- with a score >= 10 in the table second_table
 SELECT `score`, `name`
-FROM `hbtn_0c_0`
+FROM `second_table`
 WHERE score >= 10
 ORDER BY `score` DESC;
