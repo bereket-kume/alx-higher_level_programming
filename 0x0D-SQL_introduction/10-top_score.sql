540d7fb937ec1abc7de2d32b20f6e11ad7a5532e
bereket-kume <135522204+bereket-kume@users.noreply.github.com>
2024-03-12 13:55:05 -0700

    Update 10-top_score.sql

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,4 +1,5 @@
--- this is the script that lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.
+-- this is the script that lists all records of the table 
+-- second_table of the database hbtn_0c_0 in your MySQL server.
 SELECT `score`, `name`
 FROM `hbtn_0c_0`
 ORDER BY `score` DESC;
