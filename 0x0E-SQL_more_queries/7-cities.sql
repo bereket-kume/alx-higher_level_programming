b75954dd3cbe8ccd06b501fad3eec057ff548ea1
bereket kume <bereketkume@gmail.com>
2024-03-13 11:25:12 -0700

    done

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,3 +1,5 @@
+-- Write a script that creates the database hbtn_0d_usa
+--  the table cities (in the database hbtn_0d_usa) on your MySQL server.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities(
