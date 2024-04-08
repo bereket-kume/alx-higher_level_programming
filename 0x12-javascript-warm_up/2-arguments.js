caedb054c696bf90de8da2233ab908c624eb2323
bereket kume <bereketkume@gmail.com>
2024-04-08 06:35:44 -0700

    done

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 // print argument
 
-if (process.argv.length === 2){
-	console.log('No argument')
-} else if (process.argv.length === 3){
-	console.log('Argument found');
+if (process.argv.length === 2) {
+  console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
 } else {
-	console.log('Argument found')
+  console.log('Argument found');
 }
