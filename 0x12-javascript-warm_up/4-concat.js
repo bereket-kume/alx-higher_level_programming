80e2f99982675152ee2a99671a5f36ae9eb91cba
bereket kume <bereketkume@gmail.com>
2024-04-08 09:06:02 -0700

    done

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
-// script that prints two argument passed to it
+
+// script that prints two arguments passed to it
 
 const myvar = process.argv[2];
 const myvar1 = process.argv[3];
-console.log(myvar + ' is ' + myvar1);
+console.log(myvar1 + ' is ' + myvar);
