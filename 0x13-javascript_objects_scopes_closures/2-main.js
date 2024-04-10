02c32adca3d723a144529ec2fa3acbbef814c7d7
bereket kume <bereketkume@gmail.com>
2024-04-10 13:48:27 -0700

    done

--- 0x13-javascript_objects_scopes_closures/2-main.js
+++ 0x13-javascript_objects_scopes_closures/2-main.js
@@ -1,15 +1,17 @@
 #!/usr/bin/node
-const converter = require('./10-converter').converter;
+const Rectangle = require('./1-rectangle');
 
-let myConverter = converter(10);
+const r1 = new Rectangle(2, 3);
+console.log(r1);
+console.log(r1.width);
+console.log(r1.height);
 
-console.log(myConverter(2));
-console.log(myConverter(12));
-console.log(myConverter(89));
+const r2 = new Rectangle(2, -3);
+console.log(r2);
+console.log(r2.width);
+console.log(r2.height);
 
-
-myConverter = converter(16);
-
-console.log(myConverter(2));
-console.log(myConverter(12));
-console.log(myConverter(89));
+const r3 = new Rectangle(2);
+console.log(r3);
+console.log(r3.width);
+console.log(r3.height);
