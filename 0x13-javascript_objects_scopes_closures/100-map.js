d8162d64239eddbf937d7086bea4b8f66ee30ad9
bereket kume <bereketkume@gmail.com>
2024-04-10 14:15:01 -0700

    done

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
 const list = require('./100-data.js').list;
 console.log(list);
-copylist = list.map((item, index) => item * index);
-console.log(copylist);
+console.log(list.map((item, index) => item * index));
