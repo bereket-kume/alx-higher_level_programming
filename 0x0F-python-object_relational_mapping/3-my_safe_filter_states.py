fe5f12ac487a5b5bec6543e9bf8fc9133ace0828
bereket kume <bereketkume@gmail.com>
2024-04-13 00:03:29 -0700

    done

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,27 +1,27 @@
 #!/usr/bin/python3
-import MySQLdb
+"""
+Displays all values in the states table of the database hbtn_0e_0_usa
+whose name matches the one supplied as an argument. The script is safe
+from SQL injections.
+
+Usage:
+    ./3-my_safe_filter_states.py <mysql username> \
+                                <mysql password> \
+                                <database name> \
+                                <state name searched>
+"""
+
 import sys
+import MySQLdb
 
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    stat_name =  sys.argv[4]
+    # Connect to the MySQL database
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cursor = db.cursor()
 
-    connection = MySQLdb.connect(
-        host = "localhost",
-        port = 3306,
-        user = username,
-        passwd = password,
-        db = database
-    )
+    # Execute the SQL query to fetch all states
+    cursor.execute("SELECT * FROM `states`")
 
-    cursor = connection.cursor()
-    query = "SELECT * FROM states WHERE name = %s"
-    cursor.execute(query, (stat_name,))
-    results = cursor.fetchall()
-
-    for row in results:
-        print(row)
-    cursor.close()
-    connection.close()
+    # Fetch and print states whose name matches the supplied argument
+    states = cursor.fetchall()
+    [print(state) for state in states if state[1] == sys.argv[4]]
