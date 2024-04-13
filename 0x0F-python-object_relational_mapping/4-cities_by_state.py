02276a13f0611d74e81a03983a898b36ed6224ed
bereket kume <bereketkume@gmail.com>
2024-04-13 00:10:05 -0700

    done

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,31 +1,27 @@
 #!/usr/bin/python3
-import MySQLdb
+"""
+Lists all cities from the database hbtn_0e_4_usa, ordered by city ID.
+
+Usage:
+    ./4-cities_by_state.py <mysql username> <mysql password> <database name>
+"""
+
 import sys
+import MySQLdb
 
-if __name__ == '__main__':
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
+if __name__ == "__main__":
+    # Connect to the MySQL database
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cursor = db.cursor()
 
-    connection = MySQLdb.connect(
-        host = 'localhost',
-        port = 3306,
-        user = username,
-        passwd = password,
-        db = database
-    )
+    # Execute the SQL query to fetch all cities with their corresponding state
+    # names
+    cursor.execute("SELECT `c`.`id`, `c`.`name`, `s`.`name` "
+                   "FROM `cities` as `c` "
+                   "INNER JOIN `states` as `s` "
+                   "ON `c`.`state_id` = `s`.`id` "
+                   "ORDER BY `c`.`id`")
 
-    cursor = connection.cursor()
-    query = """
-        SELECT cities.id, cities.name, states.name 
-        FROM cities
-        JOIN states ON cities.state_id = states.id
-        ORDER BY cities.id ASC
-        """
-    cursor.execute(query)
-
-    results = cursor.fetchall()
-    for row in results:
-        print(row)
-    cursor.close()
-    connection.close()
+    # Fetch and print all cities
+    cities = cursor.fetchall()
+    [print(city) for city in cities]
