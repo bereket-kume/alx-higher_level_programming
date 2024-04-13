624f3866a6590887747fcc0b61b2e588a331e13a
bereket kume <bereketkume@gmail.com>
2024-04-13 00:17:11 -0700

    done

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,36 +1,31 @@
 #!/usr/bin/python3
-import MySQLdb
+"""
+Displays all cities of a given state from the states table
+of the database hbtn_0e_4_usa. The script is safe from SQL injections.
+
+Usage:
+    ./5-filter_cities.py <mysql username> \
+                         <mysql password> \
+                         <database name> \
+                         <state name searched>
+"""
+
 import sys
+import MySQLdb
 
-if __name__ == '__main__':
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    state_name = sys.argv[4]
+if __name__ == "__main__":
+    # Connect to the MySQL database
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cursor = db.cursor()
 
-    connection = MySQLdb.connect(
-        host = 'localhost',
-        port = 3306,
-        user = username,
-        passwd = password,
-        db = database
-    )
+    # Execute the SQL query to fetch all cities with their corresponding state
+    # names
+    cursor.execute("SELECT * FROM `cities` as `c` "
+                   "INNER JOIN `states` as `s` "
+                   "ON `c`.`state_id` = `s`.`id` "
+                   "ORDER BY `c`.`id`")
 
-    cursor = connection.cursor()
-    query = """
-             SELECT GROUP_CONCAT(cities.name SEPARATOR ', ')
-             FROM cities
-             JOIN states ON cities.state_id = states.id
-             WHERE states.name = %s
-             ORDER BY cities.id ASC
-            """
-    
-cursor.execute(query ,(state_name,))
-result = cursor.fetchone()
-if result:
-    print(result[0])
-else:
-    print("No cities found for the state {}".format(state_name))
-
-cursor.close()
-connection.close()
+    # Fetch and print all cities of the given state
+    cities = cursor.fetchall()
+    matching_cities = [city[2] for city in cities if city[4] == sys.argv[4]]
+    print(", ".join(matching_cities))
