804e4fc541d1ca13f4ae5604c5ebec6bc932378e
bereket kume <bereketkume@gmail.com>
2024-04-26 13:51:05 -0700

    done

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,20 @@
+import sys
+import requests
+
+if __name__ == '__main__':
+    url = "https://api.github.com/repos/{}/{}/commits".format(sys.argv[1], sys.argv[2])
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        commits = response.json()
+        try:
+            for i in range(min(10, len(commits))):
+                commit = commits[i]
+                commit_data = commit.get("commit")
+                author_name = commit_data.get("author").get("name")
+                sha = commit.get("sha")
+                print("{}: {}".format(sha, author_name))
+        except IndexError:
+            print("Not enough commits found.")
+    else:
+        print("Error: Failed to retrieve commits. Status code:", response.status_code)
