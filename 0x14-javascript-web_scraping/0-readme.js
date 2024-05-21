351ff23779baaaca677d5d4b96cdec1d7e1280e7
bereket kume <bereketkume@gmail.com>
2024-05-21 04:05:48 -0700

    done

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,11 +2,7 @@
 
 const fs = require('fs')
 
-const args = process.argv;
-const filePath = args[2]
-
-
-fs.readFile(filePath, 'utf-8', (err, data) => {
+fs.readFile(process.argv[2], 'utf-8', (err, data) => {
     if (err){
         console.log(err)
     } else {
